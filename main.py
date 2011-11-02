be00d2df9c03623510a2bcd5e9c5c7f53aa07a75
Robert Vonz <rdvonz@gmail.com>
2011-11-02 00:13:24 -0700

    Added a circle drawing function

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 import pyglet
+from pyglet import gl, graphics
+from math import pi, sin, cos
 
 window = pyglet.window.Window()
+def createCircle(radius, center, verts):
+#    x = cos(angle) = radius / y
+#    y = sin(angle) = radius / x
+#    number of side is determined from number of steps from 0 to 2*pi
+    step =  2*pi/verts
+    vertList = []
+    for i in range(0, verts):
+        vertList.append(center[0] + radius*cos(step*i))
+        vertList.append(center[1] + radius*sin(step*i))
+    circle = pyglet.graphics.vertex_list(verts, ('v2f',vertList))
+    return circle
+@window.event
+def on_draw():                      
+    for i in range(0,20):
+        createCircle(i*20, (window.width/2, window.height/2), 250).draw(pyglet.gl.GL_POINTS)
+
 
 if __name__ == '__main__':
     pyglet.app.run()
