c856d061d0d0bdc77fa87dfa1d4650972881bbc0
rdvonz <rdvonz@gmail.com>
2011-11-02 17:05:22 -0700

    reworked circle function

--- main.py
+++ main.py
@@ -3,22 +3,36 @@ from pyglet import gl, graphics
 from math import pi, sin, cos
 
 window = pyglet.window.Window()
-def createCircle(radius, center, verts):
-#    x = cos(angle)
-#    y = sin(angle)
-#    number of side is determined from number of steps from 0 to 2*pi
-    step =  2*pi/verts
-    vertList = []
-    for i in range(0, verts):
-        vertList.append(center[0] + radius*cos(step*i))
-        vertList.append(center[1] + radius*sin(step*i))
-    circle = pyglet.graphics.vertex_list(verts, ('v2f',vertList))
-    return circle
-@window.event
-def on_draw():                      
-    for i in range(0,20):
-        createCircle(i*20, (window.width/2, window.height/2), 250).draw(pyglet.gl.GL_POINTS)
+def createCircle(radius, x, y, verts):
+# x = cos(angle)
+# y = sin(angle)
+# number of sides is determined by dividing 2pi to number of steps
 
+    step = 2*pi/verts
+    #Coordinates of vertices in the circle
+    vertList = []
+    #Chooses which vertice to draw
+    index = []
+    #Append center vertice
+    vertList.append(x)
+    vertList.append(y)
+    for i in range(0, verts):
+        #Groups vertices into triangles
+        index.append(0)
+        index.append(i)
+        index.append(i+1)
+        vertList.append(x + radius*cos(step*i))
+        vertList.append(y + radius*sin(step*i))
+
+    #close the circle
+    index.append(-1)
+    index.append(0)
+    index.append(1)
+    return pyglet.graphics.vertex_list_indexed(verts+1, index, ('v2f',vertList))
+
+@window.event
+def on_draw():
+    createCircle(200, window.width/2, window.height/2, 32).draw(pyglet.gl.GL_TRIANGLES)
 
 if __name__ == '__main__':
     pyglet.app.run()
