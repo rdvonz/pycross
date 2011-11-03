c058e6815510a1ca4a67b139b467f25f0aaf902c
rdvonz <rdvonz@gmail.com>
2011-11-03 11:00:31 -0700

    The circle moves.

--- game/primitives.py
+++ game/primitives.py
@@ -12,8 +12,9 @@ class Circle(object):
     Draws a beautiful circle
     '''
 
-    def __init__(self, x, y, verts, radius, batch, group=None):
+    def __init__(self, batch, x=None, y=None, verts=None, radius=None, color=(), group=None):
         #List of points
+        self.radius = radius
         vertices = []
         
         #Order points are drawn
@@ -34,23 +35,35 @@ class Circle(object):
             indices.append(i+1)
             
             #calculate points of circle
-            vertices.append(x + radius*cos(step*i))
-            vertices.append(y + radius*sin(step*i))
+            vertices.append(x + self.radius*cos(step*i))
+            vertices.append(y + self.radius*sin(step*i))
             
+        colorIndex = []
+        for i in range(0,verts+2):
+            colorIndex.append(color[0])
+            colorIndex.append(color[1])
+            colorIndex.append(color[2])
+        self.vertex_list = batch.add_indexed(verts+2, GL_TRIANGLES, group, indices, ('v2f', vertices),('c3B', colorIndex))
         
-        self.vertex_list = batch.add_indexed(verts+2, GL_TRIANGLES, group, indices, ('v2f', vertices))
-
 class Rectangle(object):
     '''
     
     '''
     
-    def __init__(self, width, length, x, y, batch, group=None):
+    def __init__(self, batch, width=0, length=0, x=0, y=0, color=(), group=None):
         xone = x-(width/2)
         xtwo = x+(width/2)
         yone = y-(length/2)
         ytwo = y+(length/2)
+        
         vertices = (xone, yone, xone, ytwo, xtwo, ytwo, xtwo, yone)
+        
         indices = [0,1,2,0,2,3]
-        self.vertex_list = batch.add_indexed(4, GL_TRIANGLES, group, indices, ('v2f', vertices))
+        colorIndex = []
+        for i in range(0,4):
+            colorIndex.append(color[0])
+            colorIndex.append(color[1])
+            colorIndex.append(color[2])
+            
+        self.vertex_list = batch.add_indexed(4, GL_TRIANGLES, group, indices, ('v2f', vertices), ('c3B', colorIndex))
         
--- main.py
+++ main.py
@@ -5,12 +5,26 @@ batch = pyglet.graphics.Batch()
 window = pyglet.window.Window()
 pyglet.gl.glClearColor(0.2, 0.4, 0.5, 1.0)
 
-circle = primitives.Circle(window.width/2, window.height/2, 50, 50, batch=batch)
-square = primitives.Rectangle(50, 100, 200, 200, batch=batch)
+circle = primitives.Circle(x=window.width/2, 
+                           y=window.height/2, 
+                           radius=50, 
+                           verts=50,
+                           color=(255,0,0),
+                           batch=batch)
+square = primitives.Rectangle(color=(123, 132, 100), x=100, y=100, length=40, width=40, batch=batch)
 @window.event
 def on_draw():
     window.clear()
     batch.draw()
 
+def update(dt):
+    #This is probably not the correct way to do this
+    for i in range(0,len(circle.vertex_list.vertices)):
+        if i%2==0:
+            circle.vertex_list.vertices[i]+=1
+    batch.draw()
+    
+
 if __name__ == '__main__':
+    pyglet.clock.schedule_interval(update, 1/120.0)
     pyglet.app.run()
