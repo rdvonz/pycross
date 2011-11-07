2ff6077e80325986ad1c424bb6a7ec1757f826d6
Robert Vonz <rdvonz@gmail.com>
2011-11-06 20:04:07 -0800

    Moved the windowing stuff to a separate module This will be changed later, but allows access to the height of the window (useful for centering the picross)

--- main.py
+++ main.py
@@ -1,25 +1,19 @@
 import pyglet
-from game import primitives, resources, level
-
-batch = pyglet.graphics.Batch()
-window = pyglet.window.Window()
-
+from game import primitives, resources, level, window
 pyglet.gl.glClearColor(0.2, 0.4, 0.5, 1.0)
 
 mouse = pyglet.window.ImageMouseCursor(resources.cursor)
-cursor = window.set_mouse_cursor(mouse)
+cursor = window.window.set_mouse_cursor(mouse)
 
 lvl = level.Level(pyglet.image.load('resources/levels/lv2.bmp'))
 
-lvl.draw_grid(21, 200, 200, batch)
-print lvl.row_numbers
-print lvl.column_numbers
+lvl.draw_picross()
 
-@window.event
+@window.window.event
 def on_draw():
-    window.clear()
-    batch.draw()
-    window.set_mouse_cursor(cursor)
+    window.window.clear()
+    window.batch.draw()
+    window.window.set_mouse_cursor(cursor)
 
 
 def on_mouse_motion(x, y, button, modifiers):
@@ -28,7 +22,7 @@ def on_mouse_motion(x, y, button, modifiers):
 
 def update(dt):
     #This is probably not the correct way to do this)
-    window.push_handlers(on_mouse_motion)
+    window.window.push_handlers(on_mouse_motion)
 
 
 if __name__ == '__main__':
