2b0f42750b05d4042e43ea6a099affb44991ca0b
Robert Vonz <rdvonz@gmail.com>
2011-11-06 20:05:38 -0800

    New window module, will be changed later.

---
+++ game/window.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov  6 17:57:29 2011
+
+@author: -
+"""
+
+#I've found that there are certain modules that need window attributes,
+#so I'm instantiating it here for now.
+import pyglet
+
+window = pyglet.window.Window()
+batch = pyglet.graphics.Batch()
